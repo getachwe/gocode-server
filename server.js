9041835bd16352b28ce5396572a8e4001f8d2b49
getachwe <getachwe391@gmail.com>
2021-05-31 15:51:15 +0300

    first  commit

---
+++ server.js
@@ -0,0 +1,88 @@
+const { json } = require("express");
+const express = require("express");
+const fs = require("fs");
+const app = express();
+app.use(express.json());
+app.get("/products", (req, res) => {
+  const { title} = req.query;
+ 
+  fs.readFile("products.json", "utf8", (err, products) => {
+    const productsArr = JSON.parse(products);
+    if (title) {
+      const productsfiltered = productsArr.filter((product) =>
+       product.title.includes(title)
+       );
+      res.send(productsfiltered ? productsfiltered : "NO found for your query");
+    } else {
+      res.send(productsArr);
+    }
+  });
+});
+app.get("/products/:id", (req, res) => {
+  fs.readFile("products.json", "utf8", (err, products) => {
+    const productsArr = JSON.parse(products);
+    const product = productsArr.find((item) => item.id === +req.params.id);
+    if (product) {
+      res.send(product);
+    } else {
+      res.status(404);
+      res.send();
+    }
+  });
+});
+app.post("/products", (req, res) => {
+  fs.readFile("products.json", "utf8", (err, products) => {
+    const productsArr = JSON.parse(products);
+    console.log(req.body);
+    productsArr.push({
+      id: productsArr.length + 1,
+      title: req.body.title,
+      price: req.body.price,
+      description: req.body.description,
+      category: req.body.category,
+      image: req.body.image,
+    });
+    fs.writeFile("products.json", JSON.stringify(productsArr), (err) => {
+      console.log(err);
+      res.send("success");
+    });
+  });
+});
+app.put("/products/:id", (req, res) => {
+  fs.readFile("products.json", "utf8", (err, products) => {
+    const productsArr = JSON.parse(products);
+    const { title, image, category, description, price } = req.body;
+    const { id } = req.params;
+    const updatedProductsArr = productsArr.map((product) => {
+      if (product.id === +id) {
+        return {
+          ...product,
+          title,
+          image,
+          category,
+          description,
+          price,
+        };
+      } else {
+        return product;
+      }
+    });
+    fs.writeFile("products.json", JSON.stringify(updatedProductsArr), (err) => {
+      console.log(err);
+      res.send("success");
+    });
+  });
+});
+app.delete("/products/:id", (req, res) => {
+  fs.readFile("products.json", "utf8", (err, products) => {
+    const productsArr = JSON.parse(products);
+    const updatedProductsArr = productsArr.filter(
+      (product) => product.id !== +req.params.id
+    );
+    fs.writeFile("products.json", JSON.stringify(updatedProductsArr), (err) => {
+      console.log(err);
+      res.send("successs");
+    });
+  });
+});
+app.listen(7090);
